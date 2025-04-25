b7d0983a9b85e495255028244ef8e4c2e4eaf614
Clay Beach <wcbeach30@gmail.com>
2025-04-25 23:52:36 +0000

    Dangerously made the entire site without one push or update, got lost in my own head and forgot to exercise good coding habits

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+  // initiate the arrays to generate the domain names
+  function generateDomain (){
+  let pronoun = ['the', 'our','they', 'that', 'them', 'what', 'which', 'whom', 'one', 'anybody', 'everybody', 'noone'];
+  let adj = ['great', 'big', 'small', 'fast', 'slow', 'large', 'dinky', 'humongous', 'quiet', 'loud', 'medium', 'tiny', 'teeny', 'giant'];
+  let noun = ['jogger', 'racoon', 'dog', 'cat', 'snake', 'car', 'train', 'airplane', 'river', 'lake', 'mesa', 'Earth', 'Sun', 'Pluto', 'planet', 'galaxy'];
+  let extensions = ['.com', '.us', '.org', 'de', '.net']
+  
+  const randomPronoun = Math.floor(Math.random() * pronoun.length);
+  const randomAdj = Math.floor(Math.random() * adj.length);
+  const randomNoun = Math.floor(Math.random() * noun.length);
+  const randomExtensions = Math.floor(Math.random() * extensions.length);
+
+
+  const randomItemPronoun = pronoun[randomPronoun];
+  const randomItemAdj = adj[randomAdj];
+  const randomItemNoun = noun[randomNoun];
+  const randomItemExtensions = extensions[randomExtensions]
+  
+
+  document.getElementById('textOutput').innerText = ("www." + randomItemPronoun + randomItemAdj + randomItemNoun + randomItemExtensions);
+
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById('randomizeBtn')
+  .addEventListener('click', generateDomain);
 };
