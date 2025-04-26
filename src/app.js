125944f23e4e17d6f2407cefaeee0d770e6e848c
Clay Beach <wcbeach30@gmail.com>
2025-04-26 00:07:00 +0000

    Added some comments about what was going on

--- src/app.js
+++ src/app.js
@@ -13,24 +13,26 @@ import "./assets/img/4geeks.ico";
   let noun = ['jogger', 'racoon', 'dog', 'cat', 'snake', 'car', 'train', 'airplane', 'river', 'lake', 'mesa', 'Earth', 'Sun', 'Pluto', 'planet', 'galaxy'];
   let extensions = ['.com', '.us', '.org', '.de', '.net', '.biz', '.info']
   
+  // randomize the items in the array
   const randomPronoun = Math.floor(Math.random() * pronoun.length);
   const randomAdj = Math.floor(Math.random() * adj.length);
   const randomNoun = Math.floor(Math.random() * noun.length);
   const randomExtensions = Math.floor(Math.random() * extensions.length);
 
-
+  // store the randomized items in variables
   const randomItemPronoun = pronoun[randomPronoun];
   const randomItemAdj = adj[randomAdj];
   const randomItemNoun = noun[randomNoun];
   const randomItemExtensions = extensions[randomExtensions]
   
-
+    // display the results so the world can see our results
   document.getElementById('textOutput').innerText = ("www." + randomItemPronoun + randomItemAdj + randomItemNoun + randomItemExtensions);
 
 }
 
 window.onload = function() {
   //write your code here
+  // when the button is clicked show us what's up
   document.getElementById('randomizeBtn')
   .addEventListener('click', generateDomain);
 };
