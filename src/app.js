13255bbb183f4412e68f7d0d241afd18d2f99ef1
Clay Beach <wcbeach30@gmail.com>
2025-04-30 23:02:59 +0000

    Corrected the code to run as it was intended, as I misunderstood the original intent

--- src/app.js
+++ src/app.js
@@ -8,30 +8,37 @@ import "./assets/img/4geeks.ico";
 
   // initiate the arrays to generate the domain names
   function generateDomain (){
-  let pronoun = ['the', 'our','they', 'that', 'them', 'what', 'which', 'whom', 'one', 'anybody', 'everybody', 'noone'];
-  let adj = ['great', 'big', 'small', 'fast', 'slow', 'large', 'dinky', 'humongous', 'quiet', 'loud', 'medium', 'tiny', 'teeny', 'giant'];
-  let noun = ['jogger', 'racoon', 'dog', 'cat', 'snake', 'car', 'train', 'airplane', 'river', 'lake', 'mesa', 'Earth', 'Sun', 'Pluto', 'planet', 'galaxy'];
-  let extensions = ['.com', '.us', '.org', '.de', '.net', '.biz', '.info']
+  const pronoun = ['the', 'our','they', 'that', 'them', 'what', 'which', 'whom', 'one', 'anybody', 'everybody', 'noone'];
+  const adj = ['great', 'big', 'small', 'fast', 'slow', 'large', 'dinky', 'humongous', 'quiet', 'loud', 'medium', 'tiny', 'teeny', 'giant'];
+  const noun = ['jogger', 'racoon', 'dog', 'cat', 'snake', 'car', 'train', 'airplane', 'river', 'lake', 'mesa', 'Earth', 'Sun', 'Pluto', 'planet', 'galaxy'];
+  const extensions = ['.com', '.us', '.org', '.de', '.net', '.biz', '.info']
   
-  // randomize the items in the array
-  const pronounIndex = Math.floor(Math.random() * pronoun.length);
-  const adjIndex = Math.floor(Math.random() * adj.length);
-  const nounIndex = Math.floor(Math.random() * noun.length);
-  const extensionsIndex = Math.floor(Math.random() * extensions.length);
-
-  // store the randomized items in variables
-  const randomPronoun = pronoun[pronounIndex];
-  const randomAdj = adj[adjIndex];
-  const randomNoun = noun[nounIndex];
-  const randomExtensions = extensions[extensionsIndex]
   
-    // display the results so the world can see our results
-  document.getElementById('textOutput').innerText = ("www." + randomPronoun + randomAdj + randomNoun + randomExtensions);
-
+  const quads = [];
+  
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = i + 1; j < adj.length; j++) {
+      for (let k = j + 1; k < noun.length; k++) {
+        quads.push([pronoun[i] + adj[j] + noun[k]]);
+    }
+  }
 }
+
+  
+  quads.forEach((x) => {
+    for (let l = 0; l < extensions.length; l++) {
+      console.log("www."+x+extensions[l]);
+  }});
+  
+    // more statements
+  }
+
 
 window.onload = function() {
   //write your code here
+
+    // more statements
+
   // when the button is clicked show us what's up
   document.getElementById('randomizeBtn')
   .addEventListener('click', generateDomain);
